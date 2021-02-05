a4f0c58f1aff9186de8c30e994719fc86f71441b
shinji2960 <75132879+shinji2960@users.noreply.github.com>
2021-02-05 16:37:27 +0900

    Add files via upload

---
+++ 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8/script.js
@@ -0,0 +1,10 @@
+// ナビを下にスライド
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-150px";
+  }
+}
